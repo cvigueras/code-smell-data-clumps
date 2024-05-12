7651681b6bac4f8306c5ec95cf286de9b45c54db
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-05-12 17:08:04 +0200

    chore: init project.

---
+++ DataClumps.App/DataClumps.App/Booking.cs
@@ -0,0 +1,22 @@
+﻿namespace DataClumps.App
+{
+    public class Booking
+    {
+        public Booking(string name, string dni, int age, string drivingLicence, DateTime from, DateTime to)
+        {
+            Name = name;
+            DNI = dni;
+            Age = age;
+            DrivingLicence = drivingLicence;
+            From = from;
+            To = to;
+        }
+
+        public string Name { get; set; }
+        public string DNI { get; set; }
+        public int Age { get; set; }
+        public string DrivingLicence { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
---
+++ DataClumps.App/DataClumps.App/Program.cs
@@ -0,0 +1,4 @@
+﻿using DataClumps.App;
+
+var booking = new Booking("Carlos", "12345678Z", 22, "0987654567", 
+    DateTime.Now, DateTime.Now.AddDays(5));
